cc8a3e82beda445607fc842bddc3fa9905d27d57
Meathill <meathill@gmail.com>
2018-05-28 18:02:59 +0800

    change: first version of slide

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
+const {promisify} = require('util');
 const gulp = require('gulp');
 const del = require('del');
-const sequence = require('run-sequence');
+const mkdirp = require('mkdirp');
 const replace = require('gulp-replace');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
@@ -11,9 +13,14 @@ const webpackStream = require('webpack-stream');
 const {compiler, toCDN} = require('reveal-markdown-compiler');
 const PATH_REG = /\.\/node_modules\/([\w.\-]+)\//g;
 const DOC = 'docs/';
+const mkdir = promisify(mkdirp);
 
 gulp.task('clear', () => {
   return del(`${DOC}*`);
+});
+
+gulp.task('mkdir', () => {
+  return mkdir(`${DOC}`);
 });
 
 gulp.task('js', () => {
@@ -21,11 +28,15 @@ gulp.task('js', () => {
     .pipe(webpackStream(require('./webpack.config.prod'), webpack))
     .pipe(replace(PATH_REG, toCDN))
     .pipe(uglify())
-    .pipe(gulp.dest(`${DOC}js/`))
+    .pipe(gulp.dest(`${DOC}js/`));
 });
 
 gulp.task('stylus', () => {
-  return gulp.src('styl/screen.styl')
+  const styl = 'styl/screen.styl';
+  if (!fs.existsSync(styl)) {
+    return Promise.resolve(false);
+  }
+  return gulp.src(styl)
     .pipe(stylus({
       'include css': true
     }))
@@ -44,10 +55,9 @@ gulp.task('html', () => {
   return compiler('index.dev.html', 'content.md', `${DOC}index.html`);
 });
 
-gulp.task('default', taskDone => {
-  sequence(
-    'clear',
-    ['html', 'stylus', 'js', 'copy'],
-    taskDone
-  );
-});
+gulp.task('default', gulp.series(
+  'clear',
+  'mkdir',
+  gulp.parallel('html', 'stylus', 'js', 'copy', done => done()),
+  done => done(),
+));
